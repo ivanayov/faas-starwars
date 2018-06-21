cb1f1f5be469c5111ee03b2f520028ed04e135bc
Ivana Yovcheva <iyovcheva@vmware.com>
2018-06-21 21:06:46 +0300

    Fix yodish empty request
    Signed-off-by: Ivana Yovcheva <iyovcheva@vmware.com>

--- yodish/handler.py
+++ yodish/handler.py
@@ -2,8 +2,11 @@ import requests
 
 def handle(req):
     
-    r = requests.get('http://yoda-api.appspot.com/api/v1/yodish?text=' + req)
+    if req:
+        r = requests.get('http://yoda-api.appspot.com/api/v1/yodish?text=' + req)
 
-    result = r.json()
+        result = r.json()
 
-    return result["yodish"]
+        return result["yodish"]
+
+    return req
